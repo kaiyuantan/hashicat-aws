afc334e970711db65cad6de35249125d09a5f1f6
Kai Yuan Tan <kaiyuan.tky@gmail.com>
2022-10-28 06:26:00 +0000

    123

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3_bucket" {
   source  = "app.terraform.io/kaiyuantan-test/s3-bucket/aws"
   version = "2.8.0"
-  bucket = "${bucketprefix}-my-s3-bucket"
+  bucket = "${var.bucketprefix}-my-s3-bucket"
   acl    = "private"
 }
